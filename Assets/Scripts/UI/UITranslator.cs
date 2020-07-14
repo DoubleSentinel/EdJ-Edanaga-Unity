96cf4e985da135d567a27fe53cf6ccea236c8032
Karim Luy <contact@karimluy.ch>
2020-07-14 18:09:48 +0200

    added a new tag for translatable UI elements

---
+++ Assets/Scripts/UI/UITranslator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using SimpleJSON;
+using UnityEngine;
+using TMPro;
+
+public class UITranslator : MonoBehaviour
+{
+    [SerializeField] private GameObject controllers;
+
+    private BackendAPI m_api;
+
+    private JSONNode translation;
+    // Unity calls Awake after all active GameObjects in the Scene are initialized
+    void Awake()
+    {
+        // fetch translation data
+        m_api = controllers.GetComponent<BackendAPI>();
+        Dictionary<string, string> filters = new Dictionary<string, string>();
+        filters.Add("language", "EN");
+        filters.Add("scene", "test_menu");
+        m_api.ApiPull("ui", filters, ConvertJSONToObject);
+    }
+
+    private void ConvertJSONToObject(string json)
+    {
+        translation = JSON.Parse(json);
+        TranslateUI();
+    }
+
+    private void TranslateUI()
+    {
+        //TODO: YOU fookin retard, why do you think with your ass?
+        foreach (GameObject translatableObject in GameObject.FindGameObjectsWithTag("Translatable"))
+        {
+            foreach (JSONNode translatedUIElement in translation["data"][0]["elements"])
+            {
+                try
+                {
+                    Transform label = translatableObject.transform.GetChild(0);
+                    label.gameObject.GetComponent<TextMeshProUGUI>().SetText(translatedUIElement["text_value"]);
+                }
+                catch (UnityException e)
+                {
+                    translatableObject.GetComponent<TextMeshProUGUI>().SetText(translatedUIElement["text_value"]);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Utility/BackendAPI.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BackendAPI : MonoBehaviour 
+{
+    public static string BASEAPIURL = "localhost:5001";
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void ApiPull(string endpoint, Dictionary<string, string> filters, Action<string> callbackSuccess)
+    {
+        string uri = BASEAPIURL + "/" + endpoint + "/?";
+        foreach(KeyValuePair<string, string> filter in filters)
+        {
+            uri+=filter.Key + "=" + filter.Value + "&";  
+        }
+        StartCoroutine(GetRequest(uri, callbackSuccess));
+    }
+
+    IEnumerator GetRequest(string uri, Action<string> callbackSuccess)
+    {
+        using(UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            yield return webRequest.SendWebRequest();
+
+            if(webRequest.isNetworkError || webRequest.isHttpError)
+            {
+                // failed to access api error handling
+                yield break;
+            } else {
+                callbackSuccess(webRequest.downloadHandler.text);
+                yield break;
+            }
+        }
+    }
+
+}
