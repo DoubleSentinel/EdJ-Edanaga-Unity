d261b7bf917d68a939858b426a963f97233f2b09
Karim Luy <contact@karimluy.ch>
2020-08-25 17:06:39 +0200

    preliminary Ch2.2 view handling

---
+++ Assets/Scripts/Chapters/TownViewController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TownViewController : MonoBehaviour
+{
+    [SerializeField]
+    private Button btnContinue;
+
+    private int notifications;
+
+    void Awake()
+    {
+        if (btnContinue == null)
+        {
+            btnContinue = GameObject.Find("btnContinue").GetComponent<Button>();
+        }
+    }
+
+    public void UpdateObjectiveButton(GameObject caller)
+    {
+        caller.transform.GetChild(1).gameObject.SetActive(false);
+        notifications = 0;
+        foreach (Transform objective in transform.GetChild(0))
+        {
+            GameObject notification = objective.GetChild(1).gameObject;
+            if (!notification.activeSelf)
+            {
+                notifications += 1;
+                if (notifications == 10)
+                {
+                    btnContinue.interactable = true;
+                }
+            }
+        }
+    }
+    
+}
---
+++ Assets/Scripts/Gameplay/ConversationHandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SimpleJSON;
+using UnityEngine;
+
+public class ConversationHandler : MonoBehaviour
+{
+    [SerializeField] private GameObject controllers;
+    [SerializeField] private string[] conversationTitles;
+    [SerializeField] private bool isVertical = false;
+
+    private BackendAPI m_api;
+
+    private Dictionary<string, JSONNode> conversations;
+    private Dictionary<string, object> api_params;
+    
+    // Unity calls Awake after all active GameObjects in the Scene are initialized
+    void Awake()
+    {
+        conversations = new Dictionary<string, JSONNode>();
+        api_params = new Dictionary<string, object>();
+        if (controllers == null)
+        {
+            controllers = GameObject.Find("Controllers");
+        }
+        m_api = controllers.GetComponent<BackendAPI>();
+    }
+
+    public void FetchConversations()
+    {
+        foreach (string title in conversationTitles)
+        {
+            api_params.Clear();
+            api_params.Add("scene", title);
+            api_params.Add("language", controllers.GetComponent<LanguageHandler>().GetCurrentLanguage());
+            
+            m_api.ApiList("scenes", response =>
+            {
+                conversations.Add(title, JSONNode.Parse(response));
+            }, api_params);
+        }
+    }
+
+    public void GenerateConversation(string title)
+    {
+        
+    }
+
+}
