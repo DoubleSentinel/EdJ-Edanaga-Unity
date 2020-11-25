3e827fd314b741d88c4e439a42d75331cbea21c5
thierrys <thierrys72@gmail.com>
2020-11-25 15:33:27 +0100

    Add Drag&Drop Scene and create AlternativeSlot and DragDropPref scripts

---
+++ Assets/Scripts/Utility/AlternativeSlot.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AlternativeSlot : MonoBehaviour, IDropHandler {
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("OnDrop");
+        if (eventData.pointerDrag != null)
+        {
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+            Debug.Log("Vous avez donné " + eventData.pointerDrag.name + " à la variante " + name);
+        }
+    }
+}
