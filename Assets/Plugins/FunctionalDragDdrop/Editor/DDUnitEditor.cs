d7974a51bf348ffd8e57da54e818365cb0c89f93
DoubleSentinel <contact@karimluy.ch>
2020-12-01 11:00:15 +0100

    imported drag'n'drop asset

---
+++ Assets/Plugins/FunctionalDragDdrop/Editor/DDUnitEditor.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(DDUnit)), CanEditMultipleObjects]
+public class DDUnitEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DDUnit ddUnit = (DDUnit)target;
+        
+        EditorGUILayout.HelpBox("Custom editor, see the script for a wider oversight", MessageType.None);
+
+        DrawUILine();
+        EditorGUILayout.LabelField("General Options", EditorStyles.boldLabel);
+        
+        ddUnit.tag = EditorGUILayout.TextField("DDTag", ddUnit.tag);
+
+        EditorGUILayout.Space();
+        
+        //ddUnit.canSelect = EditorGUILayout.Toggle("Can Select", ddUnit.canSelect);
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("canSelect"), new GUIContent("Can Select"), true);
+
+        //ddUnit.canDrag = EditorGUILayout.Toggle("Can Drag", ddUnit.canDrag);
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("canDrag"), new GUIContent("Can Drag"), true);
+
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("enableSelectionOutline"), new GUIContent("Outline If Selected"), true);
+        
+        DrawUILine();
+        EditorGUILayout.LabelField("Drag Options", EditorStyles.boldLabel);
+
+        EditorGUILayout.LabelField("Drag Anchor");
+        //ddUnit.dragAnchor = (DDUnit.DragAnchorItems)EditorGUILayout.EnumPopup("Anchor", ddUnit.dragAnchor);
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("dragAnchor"), new GUIContent("Anchor"), true);
+        EditorGUILayout.HelpBox("Drag anchor related to the Pointer", MessageType.None);
+
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("OnDrag Size");
+        EditorGUILayout.HelpBox("set the values x, y 0 (zero) or Vector2.zero to let the value the same as before", MessageType.None);
+        //ddUnit.onDragSize = EditorGUILayout.Vector2Field("Size", ddUnit.onDragSize);
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("onDragSize"), new GUIContent("Size"), true);
+
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("OnDrag Rotation");
+        EditorGUILayout.HelpBox("set the values x, y and z 0 (zero) or Vector3.zero to let the value the same as before", MessageType.None);
+        //ddUnit.onDragRotation = EditorGUILayout.Vector3Field("Size", ddUnit.onDragRotation);
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("onDragRotation"), new GUIContent("Size"), true);
+
+        DrawUILine();
+        EditorGUILayout.LabelField("Drop Options", EditorStyles.boldLabel);
+
+        EditorGUILayout.LabelField("Options If Dropped Outside Any Vessel");
+        //ddUnit.dropOnVoid = (DDUnit.DropOnVoidItems)EditorGUILayout.EnumPopup("Option", ddUnit.dropOnVoid);
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("dropOnVoid"), new GUIContent("Option"), true);
+
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("OnDrop Size");
+        EditorGUILayout.HelpBox("set the values x, y 0 (zero) or Vector2.zero to let the value the same as before", MessageType.None);
+        EditorGUILayout.HelpBox("If the Unit is placed on a Vessel, this value will be overridden by the LayoutGroup options", MessageType.Warning);
+        //ddUnit.onDroppedSize = EditorGUILayout.Vector2Field("Size", ddUnit.onDroppedSize);
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("onDroppedSize"), new GUIContent("Size"), true);
+
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("OnDrop Rotation");
+        EditorGUILayout.HelpBox("set the values x, y and z 0 (zero) or Vector3.zero to let the value the same as before", MessageType.None);
+        //ddUnit.onDropRotation = EditorGUILayout.Vector3Field("Size", ddUnit.onDropRotation);
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("onDropRotation"), new GUIContent("Size"), true);
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    public static void DrawUILine()
+    {
+        Color color = Color.white;
+        int thickness = 1;
+        int padding = 0;
+        Rect r = EditorGUILayout.GetControlRect(GUILayout.Height(padding + thickness));
+        r.height = thickness;
+        r.y += padding / 2;
+        r.x -= 2;
+        r.width += 6;
+        EditorGUI.DrawRect(r, color);
+    }
+}
---
+++ Assets/Plugins/FunctionalDragDdrop/Editor/DDVesselEditor.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(DDVessel)), CanEditMultipleObjects]
+public class DDVesselEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DDVessel ddVessel = (DDVessel)target;
+
+        EditorGUILayout.HelpBox("Custom editor, see the script for a wider oversight", MessageType.None);
+
+        DrawUILine();
+        EditorGUILayout.LabelField("Drop In Rules", EditorStyles.boldLabel);
+
+        EditorGUILayout.LabelField("Drop In");
+        //ddVessel.dropInTagsRule = (DDVessel.DropInTagsRuleItems)EditorGUILayout.EnumPopup("Rule", ddVessel.dropInTagsRule);
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("dropInTagsRule"), new GUIContent("Rule"), true);
+
+        if (ddVessel.dropInTagsRule == DDVessel.DropInTagsRuleItems.dropInOnlyEnabledTags)
+        {
+            EditorGUILayout.HelpBox("No DDUnits can be dropped in, excep the ones with DDTags listed below", MessageType.Warning);
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("dropInEnabledTags"), true);
+        }
+        else if (ddVessel.dropInTagsRule == DDVessel.DropInTagsRuleItems.doNotDropInDisabledTags)
+        {
+            EditorGUILayout.HelpBox("All DDUnits can be dropped in, excep the ones with DDTags listed below", MessageType.None);
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("dropInDisabledTags"), true); 
+        }
+
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("Max Number of DDUnits");
+        EditorGUILayout.HelpBox("value 0 (zero) for no limit", MessageType.None);
+        //ddVessel.maxNumberOfUnits = EditorGUILayout.IntField("Value", ddVessel.maxNumberOfUnits);
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("maxNumberOfUnits"), new GUIContent("Value"), true);
+
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("Default Drop In Position");
+        //ddVessel.defaultDropPosition = (DDVessel.DefaultDropPositionItems)EditorGUILayout.EnumPopup("Option", ddVessel.defaultDropPosition);
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("defaultDropPosition"), new GUIContent("Option"), true);
+
+        string defaultDropPositionMessage = "Drop In position if vessel is empty or DDUnit is not placed over other DDUnits.";
+        if (ddVessel.defaultDropPosition == DDVessel.DefaultDropPositionItems.dropInAtIndex)
+        {
+            defaultDropPositionMessage += " Dropped DDUnit(s) are placed at Index set below";
+            EditorGUILayout.HelpBox(defaultDropPositionMessage, MessageType.None);
+            //ddVessel.defaultDropPositionIndex = EditorGUILayout.IntField("Index", ddVessel.defaultDropPositionIndex);
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("defaultDropPositionIndex"), new GUIContent("Index"), true);
+        }
+        else
+        {
+            EditorGUILayout.HelpBox(defaultDropPositionMessage, MessageType.None);
+        }
+
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("Drop In Over DDUnit");
+        EditorGUILayout.HelpBox("Option for when DDUnit is placed over or between other DDUnit(s)", MessageType.None);
+        //ddVessel.dropOverUnit = (DDVessel.DropOverUnitItems)EditorGUILayout.EnumPopup("Option", ddVessel.dropOverUnit);
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("dropOverUnit"), new GUIContent("Option"), true);
+        if (ddVessel.dropOverUnit == DDVessel.DropOverUnitItems.dropInOnGhostPosition)
+        {
+            EditorGUILayout.HelpBox("If there is a Layout Group in this GameObject, a 'GhostUnit' will indicate the Drop In position", MessageType.None);
+        }
+
+        DrawUILine();
+        EditorGUILayout.LabelField("Drag Out Rule", EditorStyles.boldLabel);
+
+        EditorGUILayout.HelpBox("All DDUnits can be dragged out, excep the ones with DDTags listed below", MessageType.None);
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("dragOutDisabledTags"), true);
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    // dar vertical line on editor gui modified form code by alexanderameye
+    public static void DrawUILine()
+    {
+        Color color = Color.white;
+        int thickness = 1;
+        int padding = 0;
+        Rect r = EditorGUILayout.GetControlRect(GUILayout.Height(padding + thickness));
+        r.height = thickness;
+        r.y += padding / 2;
+        r.x -= 2;
+        r.width += 6;
+        EditorGUI.DrawRect(r, color);
+    }
+}
