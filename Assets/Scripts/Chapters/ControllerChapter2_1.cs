2db7688693f8688f038f315b843683b8bc56d7e6
thierrys <thierrys72@gmail.com>
2020-12-15 17:08:40 +0100

    Create sceneObjectives array in the ControllerChapter2.1 script

--- Assets/Scripts/Chapters/ControllerChapter2_1.cs
+++ Assets/Scripts/Chapters/ControllerChapter2_1.cs
@@ -1,40 +1,94 @@
-﻿using System.Collections;
+﻿using Doozy.Engine;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ControllerChapter2_1 : MonoBehaviour
 {
-    [SerializeField]
-    private Button btnContinue;
+    [Header("2D Scene References")]
+    private GameObject sceneObjective;
 
-    private int notifications;
+    [SerializeField] private GameObject scenePlayer;
+    [SerializeField] private GameObject[] sceneObjectives;
+    //[SerializeField] private GameObject[] hosts;
+    private int currentCallPair;
+
+    [Header("Conversation References")] public GameObject[] ConversationBubbles;
+
+    // Local variables
+    private GameObject controllers;
+
+    // BargainConversation vars
+    [HideInInspector] public int conversationIndex = 0;
+    public ConversationHandler.ConversationEnd conversationCallback;
+
+    // BargainConversation vars
+    [HideInInspector] public int hostConversationIndex = 0;
+    [HideInInspector] public int groupedConversationIndex = 0;
+    public ConversationHandler.ConversationEnd hostConversationCallback;
+    public ConversationHandler.ConversationEnd groupedConversationCallback;
 
     void Awake()
     {
-        if (btnContinue == null)
-        {
-            btnContinue = GameObject.Find("btnContinue").GetComponent<Button>();
-        }
-        GameObject.Find("Controllers").GetComponent<LanguageHandler>().translateUI();
+        currentCallPair = -1;
+        controllers = GameObject.Find("Controllers");
+        conversationCallback = () => { GameEventMessage.SendEvent("ContinueToTown"); };
     }
 
-    public void UpdateObjectiveButton(GameObject caller)
+    private void Start()
     {
-        caller.transform.GetChild(1).gameObject.SetActive(false);
-        notifications = 0;
-        foreach (Transform objective in transform.GetChild(0))
+        controllers.GetComponent<LanguageHandler>().translateUI();
+        sceneObjectives = new GameObject[10];
+        //hosts = new GameObject[10];
+        //PrepareCall();
+    }
+
+    public void SetConversationIndex(int index)
+    {
+        conversationIndex = index;
+       
+    }
+
+    public void ClearCharacters()
+    {
+        foreach (GameObject character in GameObject.FindGameObjectsWithTag("Character"))
         {
-            GameObject notification = objective.GetChild(1).gameObject;
-            if (!notification.activeSelf)
-            {
-                notifications += 1;
-                if (notifications == 10)
-                {
-                    btnContinue.interactable = true;
-                }
-            }
+            character.transform.position = new Vector3(11, 0, 1);
+            character.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
         }
     }
-    
+
+    public void SetupHostConversation()
+    {
+        float height = Screen.height * 0.75f / 2f;
+        float depth = -1f;
+        Vector3 player = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width * 2 / 3,
+            height));
+        Vector3 host = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 4,
+            height));
+        scenePlayer.transform.position = new Vector3(player.x, player.y, depth);
+        sceneObjective.transform.position = new Vector3(host.x, host.y, depth);
+        scenePlayer.SetActive(true);
+        sceneObjective.SetActive(true);
+
+        var ch = ConversationBubbles[0].GetComponent<ConversationHandler>();
+        ch.callback = hostConversationCallback;
+        ch.GenerateConversation(hostConversationIndex);
+        ch.NextConversationSnippet();
+    }
+
+    private void StartCall()
+    {
+        ClearCharacters();
+
+        sceneObjective = sceneObjectives[conversationIndex];
+        
+        string title = "";
+        title = $"2.1.2_Dialogue_objective{conversationIndex}";
+
+        GetComponent<ConversationHandler>().GenerateConversation(title);
+        GetComponent<ConversationHandler>().NextConversationSnippet();
+    }
 }
