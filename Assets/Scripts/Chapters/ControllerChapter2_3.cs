440f8bce510e08b1098cf7b39e70f10c7fb8f080
DoubleSentinel <contact@karimluy.ch>
2020-12-17 13:35:31 +0100

    ui state machine, title and host conversation views, 2D scene base

---
+++ Assets/Scripts/Chapters/ControllerChapter2_3.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerChapter2_3 : MonoBehaviour
+{
+    [Header("2D Scene References")]
+    [SerializeField] private GameObject sceneHost;
+    [SerializeField] private GameObject scenePlayer;
+    
+    
+    [Header("Conversation References")]
+    [HideInInspector] public int hostConversationIndex = 0;
+    public GameObject[] ConversationBubbles;
+    public ConversationHandler.ConversationEnd hostConversationCallback;
+    
+    public void SetupHostConversation()
+    {
+        float height = Screen.height * 0.75f / 2f;
+        float depth = -1f;
+        Vector3 player = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width * 2 / 3,
+            height));
+        Vector3 host = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 4,
+            height));
+        scenePlayer.transform.position = new Vector3(player.x, player.y, depth);
+        sceneHost.transform.position = new Vector3(host.x, host.y, depth);
+        scenePlayer.SetActive(true);
+        sceneHost.SetActive(true);
+
+        var ch =  ConversationBubbles[0].GetComponent<ConversationHandler>();
+        ch.callback = hostConversationCallback;
+        ch.GenerateConversation(hostConversationIndex);
+        ch.NextConversationSnippet();
+    }
+}
