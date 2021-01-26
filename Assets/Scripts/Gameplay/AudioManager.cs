28c5a28e25bd7b6b90270e33b082cdd29861b964
thierrys <thierrys72@gmail.com>
2021-01-26 17:32:38 +0100

    Add AudioManger script to control the AudioMixer volume component

---
+++ Assets/Scripts/Gameplay/AudioManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioMixer _MasterMixer;
+
+    public void SetMasterVolume(Slider volume)
+    {
+        _MasterMixer.SetFloat("VolumeMaster", volume.value);
+    }
+
+    public void SetUIVolume(Slider volume)
+    {
+        _MasterMixer.SetFloat("VolumeUI", volume.value);
+    }
+
+    public void SetMusicVolume(Slider volume)
+    {
+        _MasterMixer.SetFloat("VolumeMusic", volume.value);
+    }
+
+    public void SetSFXVolume(Slider volume)
+    {
+        _MasterMixer.SetFloat("VolumeSFX", volume.value);
+    }
+}
--- Assets/Scripts/Gameplay/SoundsController.cs
+++ Assets/Scripts/Gameplay/SoundsController.cs
@@ -10,35 +10,43 @@ public class SoundsController : MonoBehaviour
     //Sound Name
     public string MySoundName;
 
-
-
     public SoundyController MyController;
-
     //The AudioClip to play
     public AudioClip Clip;
-
     //The volume of the audio source (0.0 to 1.0)
     public float Volume;
-
     //The pitch of the audio source
     public float Pitch;
-
     //Is the audio clip looping?
     public bool Loop;
-
     //Sets how much this AudioSource is affected by 3D spatialisation calculations (attenuation, doppler etc). 0.0 makes the sound full 2D, 1.0 makes it full 3D
     public float SpatialBlend = 0.0f; //Default
 
     private void Start()
     {
-        //SoundyController controller = SoundyManager.GetController();
         SoundyManager.Init();
     }
 
-    //Play specific sound
-    public void PlaySound(string MySoundDatabaseName, string MySoundName)
+    //Play specific UI sound
+    public void PlaySoundUI(string MySoundName)
     {
+        MySoundDatabaseName = "UI"; 
 	    SoundyManager.Play(MySoundDatabaseName, MySoundName);
+    }
+
+    //Play specific Ambiance sound
+    public void PlaySoundAmbiance(string MySoundName)
+    {
+        MySoundDatabaseName = "Ambiance";
+        SoundyManager.Play(MySoundDatabaseName, MySoundName);
+        print("PlayAmbiance : " + MySoundName);
+    }
+
+    public void PlaySoundMusic(string MySoundName)
+    {
+        MySoundDatabaseName = "Music";
+        SoundyManager.Play(MySoundDatabaseName, MySoundName);
+        print("PlayMusic : " + MySoundName);
     }
 
     //Stop all sounds
