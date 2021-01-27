ed9dd6f345cb268a9e11f18ee50dd098fd7cc057
thierrys <thierrys72@gmail.com>
2021-01-27 10:07:22 +0100

    Change volume.value sign in the AudioManager script

--- Assets/Scripts/Gameplay/AudioManager.cs
+++ Assets/Scripts/Gameplay/AudioManager.cs
@@ -11,26 +11,26 @@ public class AudioManager : MonoBehaviour
     //Set Master Mixer volume from the slider value
     public void SetMasterVolume(Slider volume)
     {
-        _MasterMixer.SetFloat("VolumeMaster", -volume.value);
+        _MasterMixer.SetFloat("VolumeMaster", volume.value);
     }
 
     //Set UI Mixer volume from the slider value
 
     public void SetUIVolume(Slider volume)
     {
-        _MasterMixer.SetFloat("VolumeUI", -volume.value);
+        _MasterMixer.SetFloat("VolumeUI", volume.value);
     }
 
     //Set Music Mixer volume from the slider value
 
     public void SetMusicVolume(Slider volume)
     {
-        _MasterMixer.SetFloat("VolumeMusic", -volume.value);
+        _MasterMixer.SetFloat("VolumeMusic", volume.value);
     }
 
     //Set SFX Mixer volume from the slider value
     public void SetSFXVolume(Slider volume)
     {
-        _MasterMixer.SetFloat("VolumeSFX", -volume.value);
+        _MasterMixer.SetFloat("VolumeSFX", volume.value);
     }
 }
