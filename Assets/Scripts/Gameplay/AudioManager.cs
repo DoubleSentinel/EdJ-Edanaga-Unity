a3bd6de3fbd5de3bdb7afcb4272a26a088677294
thierrys <thierrys72@gmail.com>
2021-01-27 10:02:53 +0100

    Add commentaries in the AudioManager script

--- Assets/Scripts/Gameplay/AudioManager.cs
+++ Assets/Scripts/Gameplay/AudioManager.cs
@@ -8,21 +8,27 @@ public class AudioManager : MonoBehaviour
 {
     public AudioMixer _MasterMixer;
 
+    //Set Master Mixer volume from the slider value
     public void SetMasterVolume(Slider volume)
     {
         _MasterMixer.SetFloat("VolumeMaster", -volume.value);
     }
+
+    //Set UI Mixer volume from the slider value
 
     public void SetUIVolume(Slider volume)
     {
         _MasterMixer.SetFloat("VolumeUI", -volume.value);
     }
 
+    //Set Music Mixer volume from the slider value
+
     public void SetMusicVolume(Slider volume)
     {
         _MasterMixer.SetFloat("VolumeMusic", -volume.value);
     }
 
+    //Set SFX Mixer volume from the slider value
     public void SetSFXVolume(Slider volume)
     {
         _MasterMixer.SetFloat("VolumeSFX", -volume.value);
