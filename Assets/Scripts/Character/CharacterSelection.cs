9f6447be1340979dd2d3b502c316c0e0c4486c96
thierrys <thierrys72@gmail.com>
2021-02-16 15:36:48 +0100

    Add Doozy View - SelectPlayer and create CharacterSelection.cs script

---
+++ Assets/Scripts/Character/CharacterSelection.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelection : MonoBehaviour
+{
+    [Header("UI References")]
+    [SerializeField] private TextMeshProUGUI characterName;
+    [SerializeField] private Image characterSplash;
+    [SerializeField] private Image backgroundColor;
+}
