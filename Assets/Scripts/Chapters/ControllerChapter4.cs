f39d16865458a1d540b6cfc453c8c14cc2ce4a82
thierrys <thierrys72@gmail.com>
2021-02-24 18:22:30 +0100

    Check default expression in the head Character.prefab, change SetupPictureFinsish() in the ControllerChapter4.cs to display the Swings result

--- Assets/Scripts/Chapters/ControllerChapter4.cs
+++ Assets/Scripts/Chapters/ControllerChapter4.cs
@@ -1,4 +1,6 @@
-﻿using TMPro;
+﻿using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class ControllerChapter4 : MonoBehaviour
@@ -7,7 +9,6 @@ public class ControllerChapter4 : MonoBehaviour
     [SerializeField] private GameObject scenePlayer;
     [SerializeField] private GameObject sceneJournalist;
     [SerializeField] private GameObject sceneEngineer;
-    [SerializeField] private GameObject sceneHost;
     [SerializeField] private GameObject[] sceneObjectives;
 
     // Local variables
@@ -27,34 +28,81 @@ public class ControllerChapter4 : MonoBehaviour
 
     private void SetupPictureFinsish()
     {
-        float height = Screen.height * 0.75f / 2f;
+        float heightMiddle = Screen.height * 0.75f / 2f;
+        float heightBottom = heightMiddle / 2f;
+        float heightTop = heightMiddle * 1.25f;
+        float widthSpace = Screen.width / 10; 
+
         float depth = -1f;
 
-        Vector3 player = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2f,
-            height / 2f));
-        Vector3 journalist = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width * 1f / 10f,
-           height));
-        Vector3 engineer = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width * 9f / 10f,
-            height));
-        Vector3 host = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2f,
-            height * 1.25f));
-       
-        Vector3 objectives0 = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width * 3 / 10f,
-            height));
-
-        sceneObjectives[0].transform.position = new Vector3(objectives0.x, objectives0.y, depth);
-        sceneObjectives[0].transform.localScale = new Vector3(0.75f, 0.75f, 0.75f);
-        sceneObjectives[0].transform.rotation = Quaternion.Euler(0, -180, 0);
-
+        //Default position of the Player, Journalist, engineer and host
+        Vector3 player = Camera.main.ScreenToWorldPoint(new Vector3(widthSpace*5, heightBottom));
+        Vector3 journalist = Camera.main.ScreenToWorldPoint(new Vector3(widthSpace, heightMiddle));
+        Vector3 engineer = Camera.main.ScreenToWorldPoint(new Vector3(widthSpace*9, heightMiddle));
+        
         scenePlayer.transform.position = new Vector3(player.x, player.y, depth);
         sceneJournalist.transform.position = new Vector3(journalist.x, journalist.y, depth);
         sceneEngineer.transform.position = new Vector3(engineer.x, engineer.y, depth);
-        sceneHost.transform.position = new Vector3(host.x, host.y, depth);
-
+        
         scenePlayer.SetActive(true);
         sceneJournalist.SetActive(true);
         sceneEngineer.SetActive(true);
-        sceneHost.SetActive(true);
+
+        Vector3[] positions = new[] { new Vector3(widthSpace*3, heightTop),
+                                      new Vector3(widthSpace*7, heightTop),
+                                      new Vector3(widthSpace*8, heightTop),
+                                      new Vector3(widthSpace*1, heightTop),
+                                      new Vector3(widthSpace*3, heightTop),
+                                      new Vector3(widthSpace*7, heightTop),
+                                      new Vector3(widthSpace*9, heightTop),
+                                      new Vector3(widthSpace*2, heightTop),
+                                      new Vector3(widthSpace*4, heightTop),
+                                      new Vector3(widthSpace*6, heightTop) };
+
+        //Swing classification results
+        //var results = controllers.GetComponent<TestingEnvironment>().SwingClassification;
+
+        Dictionary<string, float> results = new Dictionary<string, float>();
+        results.Add("objective0", 9f);
+        results.Add("objective1", 10f);
+        results.Add("objective2", 8f);
+        results.Add("objective3", 7f);
+        results.Add("objective4", 6f);
+        results.Add("objective5", 5f);
+        results.Add("objective6", 4f);
+        results.Add("objective7", 3f);
+        results.Add("objective8", 2f);
+        results.Add("objective9", 1f);
+
+        int index, i = 0;
+
+        // creating the visual list with the given prefab
+        foreach (KeyValuePair<string, float> result in results.OrderByDescending(x => x.Value))
+        {
+            index = int.Parse(result.Key.Last().ToString());
+
+            //Take the 2 first objectives in the Swing results
+            if (i < 2)
+            {
+                //Set position depending of the Swing classification
+                Vector3 position = Camera.main.ScreenToWorldPoint(positions[index]);
+                sceneObjectives[index].gameObject.transform.position = new Vector3(position.x, position.y, depth);
+
+                if (positions[i].x < widthSpace*5) //On the left of the Player position
+                {
+                    sceneObjectives[index].gameObject.transform.rotation = Quaternion.Euler(0, -180, 0); //Turn the body and head
+                }
+
+                //Adapt size of the objectives
+                sceneObjectives[index].gameObject.transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
+                sceneObjectives[index].gameObject.SetActive(true);
+            }
+            else
+            {
+                sceneObjectives[index].gameObject.SetActive(false);
+            }
+            i++;
+        }
     }
 
     //Prepare texts for the introduction
